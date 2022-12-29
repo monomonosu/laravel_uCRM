cc208f2119eaa740a5ef5978c7916b3e7dafddfb
monomonosu <pojinyan@yahoo.co.jp>
2022-12-29 23:13:16 +0900

    イベントコールバック。色々なコンポーネント使用。defineEmitsで親に値を返す。

---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,15 @@
+<script setup>
+import GuestLayout from '@/Layouts/GuestLayout.vue'
+import Label from '@/Components/InputLabel.vue'
+import Input from '@/Components/TextInput.vue'
+
+const emitTest = e => console.log(e);
+</script>
+
+<template>
+    <GuestLayout>
+        <Label>hoge</Label>
+        <Input modelValue="初期" @update:modelValue="emitTest">
+        </Input>
+    </GuestLayout>
+</template>
