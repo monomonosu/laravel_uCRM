80fbdcfa06f73aae4df8708185394d54308bdfc8
monomonosu <pojinyan@yahoo.co.jp>
2023-01-08 20:52:54 +0900

    Sanctum,axiosでのAPI通信。モーダルに顧客一覧表示。顧客クリックで親に情報を渡す処理。

--- resources/js/Components/MicroModal.vue
+++ resources/js/Components/MicroModal.vue
@@ -1,34 +1,95 @@
 <script setup>
-import { ref } from 'vue';
+import axios from 'axios';
+import { ref, onMounted, reactive } from 'vue';
 const isShow = ref(false);
+const search = ref('');
+const customers = reactive({})
 const toggleStatus = () => {
     isShow.value = !isShow.value;
 }
+const searchCustomers = async () => {
+    try {
+        await axios.get(`/api/searchCustomers/?search=${search.value}`)
+            .then(res => {
+                console.log(res.data);
+                customers.value = res.data;
+            })
+        toggleStatus();
+    }
+    catch (e) {
+        console.log(e);
+    }
+}
+
+const emit = defineEmits(['update:customerId']);
+const setCustomer = e => {
+    search.value = e.kana;
+    emit('update:customerId', e.id);
+    toggleStatus();
+}
+// onMounted(() => {
+//     axios.get('/api/user').then(res => {
+//         console.log(res.data);
+//         customers.value = res.data;
+//     })
+// })
 </script>
 
 <template>
     <div v-show="isShow" class="modal" id="modal-1" aria-hidden="true">
         <div class="modal__overlay" tabindex="-1" data-micromodal-close>
-            <div class="modal__container" role="dialog" aria-modal="true" aria-labelledby="modal-1-title">
+            <div class="modal__container w-2/3" role="dialog" aria-modal="true" aria-labelledby="modal-1-title">
                 <header class="modal__header">
                     <h2 class="modal__title" id="modal-1-title">
-                        Micromodal
+                        顧客検索
                     </h2>
-                    <button class="modal__close" aria-label="Close modal" data-micromodal-close @click="toggleStatus"></button>
+                    <button class="modal__close" aria-label="Close modal" data-micromodal-close
+                        @click="toggleStatus"></button>
                 </header>
                 <main class="modal__content" id="modal-1-content">
-                    <p>
-                        Try hitting the <code>tab</code> key and notice how the focus stays within the modal itself.
-                        Also, <code>esc</code> to close modal.
-                    </p>
+                    <div v-if="customers.value" class="lg:w-2/3 w-full mx-auto overflow-auto">
+                        <table class="table-auto w-full text-left whitespace-no-wrap">
+                            <thead>
+                                <tr>
+                                    <th
+                                        class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">
+                                        ID</th>
+                                    <th
+                                        class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                                        氏名</th>
+                                    <th
+                                        class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                                        カナ</th>
+                                    <th
+                                        class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                                        電話番号</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="customer in customers.value.data" :key="customer.id">
+                                    <td class="px-4 py-3">
+                                        <button @click="setCustomer({ id: customer.id, kana: customer.kana })"
+                                            type="button" class="text-blue-400">
+                                            {{ customer.id }}
+                                        </button>
+                                    </td>
+                                    <td class="px-4 py-3">{{ customer.name }}</td>
+                                    <td class="px-4 py-3">{{ customer.kana }}</td>
+                                    <td class="px-4 py-3">{{ customer.tel }}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </main>
                 <footer class="modal__footer">
-                    <button type="button" class="modal__btn modal__btn-primary" @click="toggleStatus">  Continue</button>
-                    <button type="button" class="modal__btn" data-micromodal-close
-                        aria-label="Close this dialog window" @click="toggleStatus">Close</button>
+                    <button type="button" class="modal__btn" data-micromodal-close aria-label="Close this dialog window"
+                        @click="toggleStatus">閉じる</button>
                 </footer>
             </div>
         </div>
     </div>
-    <button type="button" data-micromodal-trigger="modal-1" href='javascript:;' @click="toggleStatus">Open Modal Dialog</button>
+    <input type="text" name="customer" v-model="search"
+        class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+    <button type="button" data-micromodal-trigger="modal-1" href='javascript:;' @click="searchCustomers"
+        class="flex mx-auto text-white bg-teal-500 border-0 py-2 px-8 focus:outline-none hover:bg-teal-600 rounded text-lg">検索する</button>
 </template>
